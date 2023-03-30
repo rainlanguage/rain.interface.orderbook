a23391e13a1203fe19637ed3c30890db0966a1c4
thedavidmeister <thedavidmeister@gmail.com>
2023-03-30 16:15:08 +0400

    fix slither

--- src/IOrderBookV1.sol
+++ src/IOrderBookV1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.18;
 
 import "./ierc3156/IERC3156FlashLender.sol";
 import "rain.interface.interpreter/LibEvaluable.sol";
